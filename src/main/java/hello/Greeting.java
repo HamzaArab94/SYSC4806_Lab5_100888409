a3559242a71c609854ce1a89a01fdcfe5dd9c8e4
Hamza Arab <hamzaarab@cmail.carleton.ca>
2018-02-15 10:24:25 -0500

    Added new files to the project to build a web app

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,27 @@
+package hello;
+
+/**
+ * Created by hamzaarab on 2018-02-15.
+ */
+public class Greeting {
+
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,26 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * Created by hamzaarab on 2018-02-15.
+ */
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
+    }
+
+}
